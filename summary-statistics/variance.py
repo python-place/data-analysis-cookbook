bcc49c5dc0e280a9b545d8c204e1f60f48026a04
arisromil <arisromil@yahoo.com>
2023-11-21 15:23:11 -0700

    Checking the variance of a dataset

---
+++ summary-statistics/variance.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+covid_data = pd.read_csv("owid-covid-data.csv")
+covid_data = covid_data[['iso_code', 'continent', 'location', 'date', 'total_cases', 'new_cases']]
+
+data_variance = np.var(covid_data["new_cases"])
